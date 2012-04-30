dc6727b9bc210011d9ce798e43313dc331816d02
Arnaud Gelas <arnaudgelas@gmail.com>
2012-04-30 16:38:34 +0200

    fix style

--- itkStripTsImageFilter.h
+++ itkStripTsImageFilter.h
@@ -1,19 +1,19 @@
-/*=========================================================================                                                                                      
- *                                                                                                                                                               
- *  Copyright Insight Software Consortium                                                                                                                        
- *                                                                                                                                                               
- *  Licensed under the Apache License, Version 2.0 (the "License");                                                                                              
- *  you may not use this file except in compliance with the License.                                                                                             
- *  You may obtain a copy of the License at                                                                                                                      
- *                                                                                                                                                               
- *         http://www.apache.org/licenses/LICENSE-2.0.txt                                                                                                        
- *                                                                                                                                                               
- *  Unless required by applicable law or agreed to in writing, software                                                                                          
- *  distributed under the License is distributed on an "AS IS" BASIS,                                                                                            
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                                                                     
- *  See the License for the specific language governing permissions and                                                                                          
- *  limitations under the License.                                                                                                                               
- *                                                                                                                                                               
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  *=========================================================================*/
 
 
@@ -57,7 +57,7 @@ namespace itk
  * This ITK filter performs automatic skull stripping for 3D medical images
  *
  * It requires 3 inputs:
- * SetInput() 
+ * SetInput()
  * SetAtlasImage()
  * SetAtlasBrainMask()
  * and it outputs the brain mask for the patient image
@@ -83,66 +83,66 @@ StripTsImageFilter : public ImageToImageFilter<TImageType, TAtlasLabelType>
 {
 public:
 
-	// standard class typedefs
-	typedef StripTsImageFilter Self;
-	typedef ImageToImageFilter<TImageType,TAtlasLabelType> Superclass;
-	typedef SmartPointer<Self> Pointer;
-	typedef SmartPointer<const Self> ConstPointer;
+  // standard class typedefs
+  typedef StripTsImageFilter                              Self;
+  typedef ImageToImageFilter<TImageType,TAtlasLabelType>  Superclass;
+  typedef SmartPointer<Self>                              Pointer;
+  typedef SmartPointer<const Self>                        ConstPointer;
 
-	// method for creation through the object factory
-	itkNewMacro(Self);
+  // method for creation through the object factory
+  itkNewMacro(Self);
 
-	// run-time type information (and related methods)
-	itkTypeMacro(StripTsImageFilter, ImageToImageFilter);
+  // run-time type information (and related methods)
+  itkTypeMacro(StripTsImageFilter, ImageToImageFilter);
 
-	// display
-	void PrintSelf( std::ostream& os, Indent indent ) const;
+  // display
+  void PrintSelf( std::ostream& os, Indent indent ) const;
 
-	// image and label templates
-	typedef TImageType ImageType;
-	typedef typename ImageType::Pointer ImagePointer;
-	typedef typename ImageType::ConstPointer ImageConstPointer;
+  // image and label templates
+  typedef TImageType                        ImageType;
+  typedef typename ImageType::Pointer       ImagePointer;
+  typedef typename ImageType::ConstPointer  ImageConstPointer;
 
-	typedef TAtlasImageType AtlasImageType;
-	typedef typename AtlasImageType::Pointer AtlasImagePointer;
-	typedef typename AtlasImageType::ConstPointer AtlasImageConstPointer;
+  typedef TAtlasImageType                       AtlasImageType;
+  typedef typename AtlasImageType::Pointer      AtlasImagePointer;
+  typedef typename AtlasImageType::ConstPointer AtlasImageConstPointer;
 
-	typedef TAtlasLabelType AtlasLabelType;
-	typedef typename AtlasLabelType::Pointer AtlasLabelPointer;
-	typedef typename AtlasLabelType::ConstPointer AtlasLabelConstPointer;
+  typedef TAtlasLabelType                       AtlasLabelType;
+  typedef typename AtlasLabelType::Pointer      AtlasLabelPointer;
+  typedef typename AtlasLabelType::ConstPointer AtlasLabelConstPointer;
 
-	void SetAtlasImage( const TAtlasImageType * ptr );
-	void SetAtlasBrainMask( const TAtlasLabelType * ptr );
+  void SetAtlasImage( const TAtlasImageType * ptr );
+  void SetAtlasBrainMask( const TAtlasLabelType * ptr );
 
 
 protected:
 
-	StripTsImageFilter();
-	~StripTsImageFilter(){}
+  StripTsImageFilter();
+  ~StripTsImageFilter(){}
 
-	// does the real work
-	virtual void GenerateData();
+  // does the real work
+  virtual void GenerateData();
 
 
 private:
 
-	StripTsImageFilter(const Self&); //purposely not implemented
-	void operator=(const Self&); //purposely not implemented
+  StripTsImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
 
-	ImagePointer m_PatientImage;
-	AtlasImagePointer m_AtlasImage;
-	AtlasLabelPointer m_AtlasLabels;
+  ImagePointer      m_PatientImage;
+  AtlasImagePointer m_AtlasImage;
+  AtlasLabelPointer m_AtlasLabels;
 
-	void RescaleImages();
-	void DownsampleImage();
-	void RigidRegistration();
-	void AffineRegistration();
-	void BinaryErosion();
-	void MultiResLevelSet();
-	void PyramidFilter(int isoSpacing);
-	void InversePyramidFilter();
-	void LevelSetRefinement(int isoSpacing);
-	void UpsampleLabels();
+  void RescaleImages();
+  void DownsampleImage();
+  void RigidRegistration();
+  void AffineRegistration();
+  void BinaryErosion();
+  void MultiResLevelSet();
+  void PyramidFilter(int isoSpacing);
+  void InversePyramidFilter();
+  void LevelSetRefinement(int isoSpacing);
+  void UpsampleLabels();
 
 }; // end of class
 

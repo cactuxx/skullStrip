52eea659a544a4f787b98b40cc7e0b28686e09cc
Bill Lorensen <bill.lorensen@gmail.com>
2012-12-30 08:17:11 -0500

    ENH: Add documentation.

--- SkullStrip.remote.cmake
+++ SkullStrip.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SkullStrip
-  "Description."
+  "A class to perform automatic skull-stripping for neuroimage analysis."
   GIT_REPOSITORY https://github.com/lorensen/skullStrip.git
   GIT_TAG master
   )

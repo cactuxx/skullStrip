9d2d3de4477f3a49914f338a0c6c2a9cc02271a9
Stefan <stefan.bauer@istb.unibe.ch>
2013-01-15 16:59:38 +0100

    changed git repository

--- SkullStrip.remote.cmake
+++ SkullStrip.remote.cmake
@@ -1,6 +1,6 @@
 #teststage2
 itk_fetch_module(SkullStrip
   "A class to perform automatic skull-stripping for neuroimage analysis."
-  GIT_REPOSITORY https://github.com/cactuxx/skullStrip.git
+  GIT_REPOSITORY git://github.com/cactuxx/skullStrip.git
   GIT_TAG test-remote
   )

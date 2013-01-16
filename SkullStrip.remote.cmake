33e6741638210b8836f8502f0f086ac03b263f84
Stefan <stefan.bauer@istb.unibe.ch>
2013-01-16 16:19:04 +0100

    fiddling with tag (tag 2.0)

--- SkullStrip.remote.cmake
+++ SkullStrip.remote.cmake
@@ -1,4 +1,4 @@
-#teststage
+#teststage changed tag
 itk_fetch_module(SkullStrip
   "A class to perform automatic skull-stripping for neuroimage analysis."
   GIT_REPOSITORY git://github.com/cactuxx/skullStrip.git

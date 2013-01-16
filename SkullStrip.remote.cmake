00291840615f20986349e1bc1052d687b10a6a2e
Stefan <stefan.bauer@istb.unibe.ch>
2013-01-16 15:48:47 +0100

    deleted ambiguous tag test-remote minor change in remote module comment

--- SkullStrip.remote.cmake
+++ SkullStrip.remote.cmake
@@ -1,4 +1,4 @@
-#teststage2
+#teststage
 itk_fetch_module(SkullStrip
   "A class to perform automatic skull-stripping for neuroimage analysis."
   GIT_REPOSITORY git://github.com/cactuxx/skullStrip.git

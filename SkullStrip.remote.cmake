c59be7b673cf7a3401b1e867f089af5b5dca2f8a
Stefan <stefan.bauer@istb.unibe.ch>
2013-01-17 13:59:10 +0100

    cosmetic changes

--- SkullStrip.remote.cmake
+++ SkullStrip.remote.cmake
@@ -1,4 +1,4 @@
-#teststage changed tag
+#teststage
 itk_fetch_module(SkullStrip
   "A class to perform automatic skull-stripping for neuroimage analysis."
   GIT_REPOSITORY https://github.com/cactuxx/skullStrip.git

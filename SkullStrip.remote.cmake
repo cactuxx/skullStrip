af9f5edc62105d7a7b44747fde6287a3b8d0b2c3
Stefan <stefan.bauer@istb.unibe.ch>
2013-01-18 08:53:56 +0100

    updated links to master branch clarified instructions

--- SkullStrip.remote.cmake
+++ SkullStrip.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(SkullStrip
   "A class to perform automatic skull-stripping for neuroimage analysis."
   GIT_REPOSITORY https://github.com/cactuxx/skullStrip.git
-  GIT_TAG test-remote
+  GIT_TAG master
   )

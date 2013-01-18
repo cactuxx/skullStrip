bf7dc8a6e8d258e236ad196897a76be0228fcf88
Stefan <stefan.bauer@istb.unibe.ch>
2013-01-18 18:17:04 +0100

    added instructions for external module to install file added comments in remote module

--- SkullStrip.remote.cmake
+++ SkullStrip.remote.cmake
@@ -1,4 +1,7 @@
-#teststage
+#SkullStrip ITKv4 remote module
+#contact: Stefan Bauer, stefan.bauer at istb.unibe.ch
+#about: performs automatic skull-stripping, see Insight Journal article http://hdl.handle.net/10380/3353
+ 
 itk_fetch_module(SkullStrip
   "A class to perform automatic skull-stripping for neuroimage analysis."
   GIT_REPOSITORY https://github.com/cactuxx/skullStrip.git
